d5fbf547245ace512eecd9aad18b61b33e562c6e
JMC-Academy <bvotano@myjmc.edu.au>
2025-06-03 19:13:03 +1000

    Setup Prototype 1 defaults

---
+++ Assets/Universal/Scripts/Playground.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class Playground : GameBehaviour
+{
+    public GameObject player;
+
+    void Start()
+    {
+        ObjectX.ScaleObjectToZero(player);
+        ExecuteAfterSeconds(1, () =>
+        {
+            SetupPlayer();
+        });
+
+        ExecuteAfterFrames(3, () => print("3 frames later..."));
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+            player.GetComponent<Renderer>().material.color = ColorX.GetRandomColour();
+    }
+
+    private void SetupPlayer()
+    {
+        player.GetComponent<Renderer>().material.color = ColorX.GetRandomColour();
+        ObjectX.ScaleObjectToValue(player);
+    }
+}
